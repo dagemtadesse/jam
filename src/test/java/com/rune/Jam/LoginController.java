b48dfe4e4467e00365d0afb838030b31e589242e
altair <74534830+yeabsra-leul@users.noreply.github.com>
2022-01-17 23:26:54 +0300

    Update LoginController.java

--- src/test/java/com/rune/Jam/LoginController.java
+++ src/test/java/com/rune/Jam/LoginController.java
@@ -1,1 +1,15 @@
 
+@Controller
+public class LoginController{
+
+    @GetMapping("/login")
+    public String login(Model model, String error,) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid");
+        else{
+            return "login";
+        }
+            
+
+    }
+}
