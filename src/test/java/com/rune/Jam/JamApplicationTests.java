5634a15436820c6f4194e366ce8f50ce0cdbf19d
dagemtadesse <dagem@gmail.com>
2022-01-17 16:11:44 +0300

    spring-init

---
+++ src/test/java/com/rune/Jam/JamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rune.Jam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
