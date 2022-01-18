35eeb5461c74743f0ecf80cae0b941b099ff1c13
naomitimm <91492667+naomitimm@users.noreply.github.com>
2022-01-18 10:44:32 +0300

    .

--- src/main/java/com/rune/Jam/Channel.java
+++ src/main/java/com/rune/Jam/Channel.java
@@ -2,7 +2,7 @@ package com.rune.Jam;
 
 public class Channel {
     private String name;
-    private URL logo;
+    private String logo;
     private String description;
     private String adress;
     private String email;
