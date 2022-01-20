dae666a9706b31c88a735bcf0cf8afaf2b4a1ec5
altair <74534830+yeabsra-leul@users.noreply.github.com>
2022-01-20 19:49:31 +0300

    Update LogoutController.java

--- src/main/java/com/rune/Jam/LogoutController.java
+++ src/main/java/com/rune/Jam/LogoutController.java
@@ -2,19 +2,12 @@ package com.rune.Jam;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.ui.Model;
 
 @Controller
 public class LogoutController{
-
-    @GetMapping("/logout")
-=======
-
-@Controller
-public class LogoutController{
     @GetMapping ("/logout")
->>>>>>> 324b99530ccc9f22455d6ba6a3c9204dcfdf36ac
+
     public String logout(Model model) {
         model.addAttribute("message", "You have been logged out successfully");
         return "login";
