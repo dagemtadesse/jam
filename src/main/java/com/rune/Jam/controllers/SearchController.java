3308792709a0ff3fb9d76c23a58b51f5786a2239
naomitimm <91492667+naomitimm@users.noreply.github.com>
2022-01-23 20:01:25 +0300

    thymeleaf

--- src/main/java/com/rune/Jam/controllers/SearchController.java
+++ src/main/java/com/rune/Jam/controllers/SearchController.java
@@ -1,5 +1,10 @@
 package com.rune.Jam.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.rune.Jam.models.Channel;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SearchController {
     @GetMapping ("/search")
     public String search(Model model){
-    
+        model.addAttribute("naomi", Arrays.asList("naomi", "dagim"));
+
+        // List<Channel> searchResult = Arrays.asList();
+        
         return "search";
     }
 }
