99c40521e233147d69bf7a4be4369a26f9c0eb29
dagemtadesse <dagem@gmail.com>
2022-01-24 01:05:56 +0300

    posts

--- src/main/java/com/rune/Jam/JamApplication.java
+++ src/main/java/com/rune/Jam/JamApplication.java
@@ -36,11 +36,12 @@ public class JamApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		var AAU = new Channel("AAU", null, "Addis Ababa University", "AA", "aau@aau.edu.et", true );
-//		var BDU = new Channel("BDU", null, "Bahir Dar University", "BD", "bdu@bdu.edu.et", false );
-//		var AAiT = new Channel("AAiT", null, "Addis Ababa Institute of Technology", "AA", "aau@aau.edu.et", true );
+		var AAU = new Channel("AAU", null, "Addis Ababa University", "AA", "aau@aau.edu.et", true );
+		var BDU = new Channel("BDU", null, "Bahir Dar University", "BD", "bdu@bdu.edu.et", false );
+		var AAiT = new Channel("AAiT", null, "Addis Ababa Institute of Technology", "AA", "aau@aau.edu.et", true );
 //
-//
+		AAiT.setParentChannel(AAU);
+
 //		var dagem = new User("Dagem Tadesse", "dagem", "dagem@gmail.com", "+251 999 999 999", "student", User.Role.REGULAR);
 //		var naomi = new User("Naomi", "naomi", "naomi@gmail.com", "+251 999 999 999", "student", User.Role.ADMIN);
 //		var yeabsra = new User("Yeabsra", "yeab", "yl@gmail.com", "+251 999 999 999", "student", User.Role.REGULAR);
@@ -51,17 +52,17 @@ public class JamApplication implements CommandLineRunner {
 //		AAiT.setCreator(naomi);
 //		AAiT.setParentChannel(AAU);
 //
-//		var first = new Post("First post oh how the food is terible in AAU", dagem, AAU);
+		var first = new Post("First post oh how the food is terible in AAU", null, AAU);
 //		var firstComment = new Comment("Really, the food in AAit it good", first, naomi);
 //		var secondComment = new Comment("The food is so awful man", first, yeabsra);
 //
-//		var second = new Post("Quick update tomorrow i'm planning on mass shouting. Come and watch the spectacle",  dagem, AAU);
+		var second = new Post("Quick update tomorrow i'm planning on mass shouting. Come and watch the spectacle",  null, AAU);
 //		var thirdComment = new Comment("sike", second, dagem);
-//		var third = new Post("new Event in AAit",  hela, AAiT);
+		var third = new Post("new Event in AAit",  null, AAiT);
 //
 //		userRepository.saveAll(Arrays.asList(dagem, naomi, yeabsra, tsega, hela));
-//		channelRepository.saveAll(Arrays.asList(AAU, AAiT, BDU));
-//		postRepository.saveAll(Arrays.asList(first, second, third));
+		channelRepository.saveAll(Arrays.asList(AAU, AAiT, BDU));
+		postRepository.saveAll(Arrays.asList(first, second, third));
 //		commentRepository.saveAll(Arrays.asList(firstComment, secondComment, thirdComment));
 //
 //		naomi.setBookmarks(Arrays.asList(AAU, AAiT));
