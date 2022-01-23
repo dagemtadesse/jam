3b4112118956732c5615ac07be6a420908d1e845
naomitimm <91492667+naomitimm@users.noreply.github.com>
2022-01-23 11:22:43 +0300

    update

---
+++ src/main/java/com/rune/Jam/controllers/CommentController.java
@@ -0,0 +1,12 @@
+package com.rune.Jam.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommentController {
+    @GetMapping("/comment")
+    public String comment(){
+        return "comment";
+    }
+}
