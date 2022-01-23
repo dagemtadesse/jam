3c241984279ed8eef43050c66633c28219f0ceda
altair <74534830+yeabsra-leul@users.noreply.github.com>
2022-01-24 01:53:04 +0300

    Update RegistrationForm.java

--- src/main/java/com/rune/Jam/models/RegistrationForm.java
+++ src/main/java/com/rune/Jam/models/RegistrationForm.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class RegistrationForm {
